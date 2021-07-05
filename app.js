dc29fec3ad5a6905800247af795279a45e039d99
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-07-05 22:01:23 +0300

    done with JS

--- app.js
+++ app.js
@@ -7,34 +7,45 @@ alert('please reply with yes/y or no/n')
 let guessedAge = prompt('I\'m I older than 20 years old?');
 if(guessedAge.toLowerCase() == 'yes' || guessedAge.toLowerCase() == 'y'){
     alert('you\'re right, actually i\'m 23 years old');
+    // console.log('right');
 } else {
     alert('sorry! you\'re wrong');
+    // console.log('wrong');
 }
 
 let guessedDrink = prompt('is coffee my favorite drink?');
 if(guessedDrink.toLowerCase() == 'yes' || guessedDrink.toLowerCase() == 'y'){
     alert('WRONG! i\'d rather nescafe');
+    // console.log('wrong');
+
 } else {
     alert('you\'re right! coffee isn\'t my favorite');
+    // console.log('right');
 }
 
 let guessedStatus = prompt('I\'m I employed these days?');
 if(guessedStatus.toLowerCase() == 'yes' || guessedStatus.toLowerCase() == 'y'){
     alert('WRONG! i\'m taking a bootcamp with ASAC');
+    // console.log('wrong');
 } else {
     alert('you\'re right! i\'m not employed');
+    // console.log('right');
 }
 
 let guessedMovie = prompt('is the godfather my favorite movie?');
 if(guessedMovie.toLowerCase() == 'yes' || guessedMovie.toLowerCase() == 'y'){
     alert('Correct! what else could be');
+    // console.log('right');
 } else {
     alert('WRONG!');
+    // console.log('wrong');
 }
 
 let guessedDish = prompt('is mansaf my favorite dish?');
 if(guessedDish.toLowerCase() == 'yes' || guessedDish.toLowerCase() == 'y'){
     alert('Correct! what else could be');
+    // console.log('right');
 } else {
     alert('WRONG! what else could be');
+    // console.log('wrong');
 }
