9f09ae1f8638a733e2285164f3f9838ea4ef0250
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-07-05 22:04:36 +0300

    done with JS

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let userName = prompt('Welcome! what\'s your name?');
 alert(`Hello ${userName}, let's play the guessing game`);
 alert('please reply with yes/y or no/n')
 
+let mark = 0;
 
 let guessedAge = prompt('I\'m I older than 20 years old?');
 if(guessedAge.toLowerCase() == 'yes' || guessedAge.toLowerCase() == 'y'){
     alert('you\'re right, actually i\'m 23 years old');
     // console.log('right');
+    mark++;
 } else {
     alert('sorry! you\'re wrong');
     // console.log('wrong');
@@ -21,6 +23,7 @@ if(guessedDrink.toLowerCase() == 'yes' || guessedDrink.toLowerCase() == 'y'){
 } else {
     alert('you\'re right! coffee isn\'t my favorite');
     // console.log('right');
+    mark++;
 }
 
 let guessedStatus = prompt('I\'m I employed these days?');
@@ -30,12 +33,14 @@ if(guessedStatus.toLowerCase() == 'yes' || guessedStatus.toLowerCase() == 'y'){
 } else {
     alert('you\'re right! i\'m not employed');
     // console.log('right');
+    mark++;
 }
 
 let guessedMovie = prompt('is the godfather my favorite movie?');
 if(guessedMovie.toLowerCase() == 'yes' || guessedMovie.toLowerCase() == 'y'){
     alert('Correct! what else could be');
     // console.log('right');
+    mark++;
 } else {
     alert('WRONG!');
     // console.log('wrong');
@@ -45,7 +50,10 @@ let guessedDish = prompt('is mansaf my favorite dish?');
 if(guessedDish.toLowerCase() == 'yes' || guessedDish.toLowerCase() == 'y'){
     alert('Correct! what else could be');
     // console.log('right');
+    mark++;
 } else {
     alert('WRONG! what else could be');
     // console.log('wrong');
 }
+
+alert(`you're mark: ${mark}, good job ${userName}`)
